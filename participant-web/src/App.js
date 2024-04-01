7d9a94c1d285992b5fd5645cb174fdd134f1182c
Steffen Hæren <steffenhaeren@gmail.com>
2024-04-01 22:33:04 +0200

    init react app

---
+++ participant-web/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Participant Web!
+      </header>
+    </div>
+  );
+}
+
+export default App;
