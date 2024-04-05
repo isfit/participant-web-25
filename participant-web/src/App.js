5d371e4a8e77f9d387a19487a97ea9ca21548cfa
Anne B. Nummedal <anneb.nummedal@dhcp-10-22-59-240.wlan.ntnu.no>
2024-04-05 17:35:50 +0200

    test123

--- participant-web/src/App.js
+++ participant-web/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Participant Web!
+        Participant Web! ISFIT!!!!!!!!
       </header>
     </div>
   );
