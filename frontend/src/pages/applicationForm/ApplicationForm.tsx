6e1a8fae06e2f84376684a32768dca0997c6a75e
Anne Nummedal <annebnu@stud.ntnu.no>
2024-04-12 17:02:37 +0200

    Made application form:)

---
+++ frontend/src/pages/applicationForm/ApplicationForm.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import {Button} from '@radix-ui/themes';
+
+interface FormValues {
+  coverLetter: string;
+}
+
+const ApplicationForm: React.FC = () => {
+  const [formValues, setFormValues] = useState<FormValues>({
+    coverLetter: ''
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormValues(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('Form submitted:', formValues);
+    // You can perform form submission logic here
+  };
+
+  return (
+    <div>
+      <h2>Application Form For Participants</h2>
+      <p>Write about yourself and why you want to join Isfit:</p>
+      <form onSubmit={handleSubmit}>
+        <div>
+            <label htmlFor="coverLetter">Cover letter:</label><br />
+          <textarea
+            id="coverLetter"
+            name="coverLetter"
+            value={formValues.coverLetter}
+            onChange={handleChange}
+            required
+            rows={20}
+            style={{ width: '100%'}} // Set width and height using inline style
+          />
+        </div>
+        <Button type="submit">Submit</Button>
+      </form>
+    </div>
+  );
+};
+
+export default ApplicationForm;
