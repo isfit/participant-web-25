5595d6bccaab07a24d14260fbb0535c2bccb6fef
Anne Nummedal <annebnu@stud.ntnu.no>
2024-04-12 18:14:47 +0200

    AboutPage is done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CreateUser from './pages/createUser/CreateUser';
 import Login from './pages/login/Login';
 import Home from './pages/home/Home';
+import AboutPage from './pages/aboutPage/AboutPage';
+
 //import NotFoundPage from './pages/NotFoundPage';
 
 const App: React.FC = () => {
@@ -13,6 +15,8 @@ const App: React.FC = () => {
       <Route path="/" element={<Home />} />
         <Route path="/createUser" element={<CreateUser />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/aboutPage" element={<AboutPage />} />
+        <Route path="/home" element={<Home />} />
         {/* Add more routes here if needed */}
         {/* <Route path="*" element={<NotFoundPage />} /> */}
       </Routes>
